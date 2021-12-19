f353ac9f7f3fea724c51e2d11be6fa66a0f9554b
akadusei <attakusiadusei@gmail.com>
2021-12-19 11:23:03 +0000

    Remove redundant operation callback method
    Setting the value to `nil` if not set is what operations already do.

--- src/avram/operations/activate.cr
+++ src/avram/operations/activate.cr
@@ -2,7 +2,6 @@ module Lucille::Activate
   macro included
     before_save do
       set_default_active_at
-      set_default_inactive_at
     end
 
     include Lucille::ValidateStatus
@@ -10,11 +9,6 @@ module Lucille::Activate
     private def set_default_active_at
       return if active_at.value
       active_at.value = Time.utc
-    end
-
-    private def set_default_inactive_at
-      return if inactive_at.value
-      inactive_at.value = nil
     end
   end
 end
