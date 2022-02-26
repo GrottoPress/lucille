c5b2bdd40ae0c81bdbc9a5d0c92f8f0fa079d983
akadusei <attakusiadusei@gmail.com>
2022-02-26 05:37:22 +0000

    Refactor `Lucille::JSON` to use *Hapi*
    See <https://github.com/GrottoPress/hapi>.

--- src/lucille/json.cr
+++ src/lucille/json.cr
@@ -1,21 +1,7 @@
+require "hapi/resource"
+
 module Lucille::JSON
   macro included
-    include JSON::Serializable
-    include JSON::Serializable::Unmapped
-
-    forward_missing_to json_unmapped
-
-    def merge(other : self) : self
-      merge JSON.parse(other.to_json).as_h
-    end
-
-    def merge(**other) : self
-      merge(other.to_h)
-    end
-
-    def merge(other : Hash) : self
-      hash = JSON.parse(to_json).as_h.merge(other)
-      self.class.from_json(hash.to_json)
-    end
+    include Hapi::Resource
   end
 end
