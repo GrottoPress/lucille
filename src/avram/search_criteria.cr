8f43b2e98764ca8837c7b726bc879f5942a270ff
akadusei <attakusiadusei@gmail.com>
2022-05-11 21:13:27 +0000

    Fix potential SQL injection in search criteria
    Related to commit 8a22283ac94c7706ec01a9abb742ff7518d4c67f.

--- src/avram/search_criteria.cr
+++ src/avram/search_criteria.cr
@@ -2,9 +2,9 @@ class String::Lucky::Criteria(T, V) < Avram::Criteria(T, V)
   def search(for keywords)
     split = keywords.split.join('%')
     pattern = "%#{split}%"
-    leading = "#{split}%"
+    leading = PG::EscapeHelper.escape_literal("#{split}%")
 
-    ilike(pattern).order_by("#{column} ILIKE '#{leading}'", :desc)
+    ilike(pattern).order_by("#{column} ILIKE #{leading}", :desc)
       .order_by("length(#{column})", :asc)
   end
 end
