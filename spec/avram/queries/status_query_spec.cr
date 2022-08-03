e88b817dd4fa529f4c6cbaecbfdd743fa5335de7
akadusei <attakusiadusei@gmail.com>
2022-08-03 09:51:48 +0000

    Refactor status query spec

--- spec/avram/queries/status_query_spec.cr
+++ spec/avram/queries/status_query_spec.cr
@@ -4,70 +4,110 @@ describe Lucille::StatusQuery do
   it "retrieves active user" do
     UserFactory.create
 
-    UserQuery.new.is_active.first?.should be_a(User)
-    UserQuery.new.is_inactive.first?.should be_nil
-    UserQuery.new.is_pending.first?.should be_nil
-    UserQuery.new.is_unactive.first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active.any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive.any?.should be_false
 
     inactive_at = 2.days.from_now.to_utc.at_beginning_of_second
     UserQuery.new.update(inactive_at: inactive_at)
 
-    UserQuery.new.is_active.first?.should be_a(User)
-    UserQuery.new.is_inactive.first?.should be_nil
-    UserQuery.new.is_pending.first?.should be_nil
-    UserQuery.new.is_unactive.first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active.any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive.any?.should be_false
 
-    UserQuery.new.is_active(inactive_at).first?.should be_nil
-    UserQuery.new.is_inactive(inactive_at).first?.should be_a(User)
-    UserQuery.new.is_pending(inactive_at).first?.should be_nil
-    UserQuery.new.is_unactive(inactive_at).first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active(inactive_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive(inactive_at).any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending(inactive_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive(inactive_at).any?.should be_false
   end
 
   it "retrieves future active user" do
     active_at = 2.days.from_now.to_utc.at_beginning_of_second
     UserFactory.create &.active_at(active_at)
 
-    UserQuery.new.is_active.first?.should be_nil
-    UserQuery.new.is_inactive.first?.should be_nil
-    UserQuery.new.is_pending.first?.should be_a(User)
-    UserQuery.new.is_unactive.first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending.any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive.any?.should be_false
 
-    UserQuery.new.is_active(active_at).first?.should be_a(User)
-    UserQuery.new.is_inactive(active_at).first?.should be_nil
-    UserQuery.new.is_pending(active_at).first?.should be_nil
-    UserQuery.new.is_unactive(active_at).first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active(active_at).any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive(active_at).any?.should be_false
 
     inactive_at = 4.days.from_now.to_utc.at_beginning_of_second
     UserQuery.new.update(inactive_at: inactive_at)
 
-    UserQuery.new.is_active.first?.should be_nil
-    UserQuery.new.is_inactive.first?.should be_nil
-    UserQuery.new.is_pending.first?.should be_a(User)
-    UserQuery.new.is_unactive.first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending.any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive.any?.should be_false
 
-    UserQuery.new.is_active(active_at).first?.should be_a(User)
-    UserQuery.new.is_inactive(active_at).first?.should be_nil
-    UserQuery.new.is_pending(active_at).first?.should be_nil
-    UserQuery.new.is_unactive(active_at).first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active(active_at).any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive(active_at).any?.should be_false
 
-    UserQuery.new.is_active(inactive_at).first?.should be_nil
-    UserQuery.new.is_inactive(inactive_at).first?.should be_a(User)
-    UserQuery.new.is_pending(inactive_at).first?.should be_nil
-    UserQuery.new.is_unactive(inactive_at).first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active(inactive_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive(inactive_at).any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending(inactive_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive(inactive_at).any?.should be_false
   end
 
   it "retrieves future inactive user" do
     active_at = 2.days.from_now.to_utc.at_beginning_of_second
     UserFactory.create &.active_at(active_at).inactive_at(active_at)
 
-    UserQuery.new.is_active.first?.should be_nil
-    UserQuery.new.is_inactive.first?.should be_nil
-    UserQuery.new.is_pending.first?.should be_nil
-    UserQuery.new.is_unactive.first?.should be_a(User)
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending.any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive.any?.should be_true
 
-    UserQuery.new.is_active(active_at).first?.should be_nil
-    UserQuery.new.is_inactive(active_at).first?.should be_a(User)
-    UserQuery.new.is_pending(active_at).first?.should be_nil
-    UserQuery.new.is_unactive(active_at).first?.should be_nil
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_active(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_inactive(active_at).any?.should be_true
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_pending(active_at).any?.should be_false
+    # ameba:disable Performance/AnyInsteadOfEmpty
+    UserQuery.new.is_unactive(active_at).any?.should be_false
   end
 end
