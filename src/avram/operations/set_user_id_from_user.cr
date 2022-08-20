22fd16da0c7d1fb10441a045a7336a9a49519616
akadusei <attakusiadusei@gmail.com>
2022-08-20 09:53:08 +0000

    Remove `SetUserIdFromUser#user!`
    Merged its functionality into `SetUserIdFromUser#user`.

--- src/avram/operations/set_user_id_from_user.cr
+++ src/avram/operations/set_user_id_from_user.cr
@@ -6,8 +6,10 @@ module Lucille::SetUserIdFromUser
       set_user_id
     end
 
-    def user! : User?
-      user || user_id.value.try { |value| UserQuery.new.id(value).first? }
+    def user : User?
+      @user ||= user_id.value.try do |value|
+        UserQuery.new.id(value).first?
+      end
     end
 
     private def set_user_id
