f90e2383843b4b1228df8493fce91715cb35a508
akadusei <attakusiadusei@gmail.com>
2022-12-31 19:10:58 +0000

    Define `Lucille::UserFromUserId#user` only if not already defined

--- src/avram/operations/user_from_user_id.cr
+++ src/avram/operations/user_from_user_id.cr
@@ -1,7 +1,9 @@
 module Lucille::UserFromUserId
   macro included
-    getter user : User? do
-      user_id.value.try { |value| UserQuery.new.id(value).first? }
-    end
+    {% unless @type.methods.find(&.name.== :user.id) %}
+      getter user : User? do
+        user_id.value.try { |value| UserQuery.new.id(value).first? }
+      end
+    {% end %}
   end
 end
