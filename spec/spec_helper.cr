418d3c84c5f515fcd8895955f3c4ec8b0786983c
akadusei <attakusiadusei@gmail.com>
2023-10-30 22:11:41 +0000

    Move transactional specs setup to database setup file

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -7,11 +7,10 @@ require "./setup/**"
 require "../src/spec"
 require "../src/cockroach"
 
-include Carbon::Expectations
-include Lucky::RequestExpectations
-
-Avram::SpecHelper.use_transactional_specs(AppDatabase)
+Habitat.raise_if_missing_settings!
 
 Avram::Migrator::Runner.new.ensure_migrated!
 Avram::SchemaEnforcer.ensure_correct_column_mappings!
-Habitat.raise_if_missing_settings!
+
+include Carbon::Expectations
+include Lucky::RequestExpectations
