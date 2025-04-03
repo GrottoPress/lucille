9583f4295c9447cddbff5ffb2b77adc5cf0daeba
akadusei <attakusiadusei@gmail.com>
2025-04-03 17:34:06 +0000

    Revert "Add support for running multiple server sockets (workers)"
    This reverts commit d077a541080938fe3e5a9a9efffecaa087d2d76b.
    
    See <https://github.com/luckyframework/lucky/issues/1806#issuecomment-2776480543>.

--- src/lucky/base_app_server.cr
+++ src/lucky/base_app_server.cr
@@ -1,15 +1,5 @@
 abstract class Lucky::BaseAppServer
   def listen : Nil
-    workers.times { server.bind_tcp(host, port, reuse_port ) }
-    server.listen
-  end
-
-  # Override this to change number of server sockets to run
-  def workers : Int32
-    1
-  end
-
-  private def reuse_port
-    workers > 1
+    server.listen(host, port)
   end
 end
