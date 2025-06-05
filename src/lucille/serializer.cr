e51c7accee861146cf5186ae81f9cf162dc3a598
akadusei <attakusiadusei@gmail.com>
2025-06-05 17:00:15 +0000

    Update code comment

--- src/lucille/serializer.cr
+++ src/lucille/serializer.cr
@@ -1,5 +1,7 @@
 # This duplicates `Lucky::Serializer`, so you can create
 # serializers with `struct`s instead of `class`es
+#
+# If you're on Lucky v1.4+, you should use `Lucky::Serializable` instead.
 module Lucille::Serializer
   macro included
     def to_json(io)
