6e32d88b0a38dab16947734c01d3ea7c76e52ccd
akadusei <attakusiadusei@gmail.com>
2026-03-27 00:17:43 +0000

    Improve performance of macro calls

--- src/avram/operations/user_from_user_id.cr
+++ src/avram/operations/user_from_user_id.cr
@@ -1,6 +1,6 @@
 module Lucille::UserFromUserId
   macro included
-    {% unless @type.methods.find(&.name.== :user.id) %}
+    {% unless @type.methods.any?(&.name.== :user.id) %}
       getter user : User? do
         user_id.value.try { |value| UserQuery.new.id(value).first? }
       end
