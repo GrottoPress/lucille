d09b8a01a8f51a9deec548db299a792753f1c211
akadusei <attakusiadusei@gmail.com>
2026-08-28 00:40:04 +0000

    Add specs for `FakeParams#get?` and `#get_all?`

--- spec/avram/fake_params_spec.cr
+++ spec/avram/fake_params_spec.cr
@@ -9,4 +9,22 @@ describe FakeParams do
 
     FakeParams.new.get?("none").should be_nil
   end
+
+  describe "#get?" do
+    it "returns string for a given key" do
+      params = params({name: "Alesia"})
+
+      params.get?(:name).should eq("Alesia")
+      params.get?(:missing).should be_nil
+    end
+  end
+
+  describe "#get_all?" do
+    it "returns array for a given key" do
+      params = params({files: ["photo.jpg"]})
+
+      params.get_all?(:files).should eq(["photo.jpg"])
+      params.get_all?("missing").should be_nil
+    end
+  end
 end
