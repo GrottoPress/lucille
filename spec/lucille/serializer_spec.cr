0a99674feb921d776581d5fa5542de5bcf0b4a5f
akadusei <attakusiadusei@gmail.com>
2026-09-23 14:05:40 +0000

    Remove test for `.for_collection`
    Missed this in commit 5ae5891ca7896ff6e9106e37c3b6e8a4530d8dee.

--- spec/lucille/serializer_spec.cr
+++ spec/lucille/serializer_spec.cr
@@ -5,13 +5,4 @@ describe Lucille::Serializer do
     user = UserFactory.create
     UserSerializer.new(user).to_json.should eq(%({"id":#{user.id}}))
   end
-
-  it "serializes records" do
-    user = UserFactory.create
-
-    UserSerializer.for_collection([user])
-      .first?
-      .try(&.to_json)
-      .should(eq %({"id":#{user.id}}))
-  end
 end
--- spec/support/app/src/serializers/base_serializer.cr
+++ spec/support/app/src/serializers/base_serializer.cr
@@ -1,7 +1,3 @@
 abstract struct BaseSerializer
   include Lucille::Serializer
-
-  def self.for_collection(collection : Enumerable, *args, **named_args)
-    collection.map { |object| new(object, *args, **named_args) }
-  end
 end
